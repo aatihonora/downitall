a7402d953301bf8e13871153e3b74fdbd8a1b3ea
Behram Akhtar <58216695+aatihonora@users.noreply.github.com>
2024-09-17 10:50:38 +0500

    Update __init__.py

--- resources/__init__.py
+++ resources/__init__.py
@@ -4,5 +4,4 @@ from resources.animesources import Animesources
 from resources.booksources import Booksources
 from resources.mangasources import Mangasources
 from resources.musicsources import Musicsources
-from resources.schoolsources import Schoolsources
 from resources.tvsources import Tvsources
