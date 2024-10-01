0c0946585c5f1e08e710ceb17b6cf03ff144969a
Behram <behramakhtar2000@gmail.com>
2024-10-02 01:56:51 +0500

    Completed the cli app with direct command

--- resources/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Importing Booksources from booksources in resource directory"""
-
-from resources.animesources import Animesources
-from resources.booksources import Booksources
-from resources.mangasources import Mangasources
-from resources.musicsources import Musicsources
-from resources.tvsources import Tvsources
